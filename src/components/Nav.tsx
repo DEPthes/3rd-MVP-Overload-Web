e18fe60739e84d83950b6aa13233fef7737fee4a
soyeon <soyeon20031010@gmail.com>
2024-08-08 22:32:00 +0900

    fix: navbar logo->mainpage 이동 추가

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,20 +6,23 @@ import searchImg from "../images/search.png";
 import insta from "../images/Insta.png";
 import writeBtn from "../images/writePostBtn.png";
 import devider from "../images/Devider.png";
+import { Link } from 'react-router-dom';
 
 const Nav: React.FC<{ profile?: string }> = (props) => {
   return (
     <nav className="navbar">
         {/* 로고 부분 */}
       <div className="navbar-left">
-        <img className="logo" src={logo} alt="Logo" />
+        <Link to="/mainPage">
+          <img className="logo" src={logo} alt="Logo" />
+        </Link>
       </div>
 
         {/* 검색, 인스타, 글작성, 프로필 부분 */}
       <div className="navbar-right">
-        <a href='#'><img className="nav-icon search-icon" src={ searchImg }/></a>
+        <a href="/mainPage"><img className="nav-icon search-icon" src={ searchImg }/></a>
         <a href='https://www.instagram.com/depth_mju.co.kr/'><img className="nav-icon instagram-icon" src={ insta }/></a>
-        <a href='#'><img className="nav-icon" src={ devider }/></a>
+        <img className="nav-icon" src={ devider }/>
         <a href='#'><img className="nav-icon write-button" src={ writeBtn }/></a>
         
         {/* 프로필 부분, 프로필 있을 시 자신의 프로필을, 없을 시 프로필 아이콘 */}
