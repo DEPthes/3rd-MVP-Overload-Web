75fb013de1245559bc1d4e9a6c6a70d1573ee965
si14444 <a1@1ui-MacBookAir.local>
2024-08-20 17:37:30 +0900

    feat/이미지 첨부 버튼

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import SaveFolder from "../assets/saveFolder.svg?react";
 import NextButton from "../assets/next.button.svg?react";
 import SaveModalSvg from "../assets/saveModal.svg?react";
+import AddImageButtonSvg from "../assets/addImageButton.svg?react";
 
-export { SaveFolder, NextButton, SaveModalSvg };
+export { SaveFolder, NextButton, SaveModalSvg, AddImageButtonSvg };
